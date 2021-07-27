209e291fc4bc82a5c835d484ad13672315d55f77
dimass30 <dimassetiaji30@gmail.com>
2021-07-27 18:50:11 +0700

    gsap

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,6 +6,45 @@ document.addEventListener( 'DOMContentLoaded', function () {
 
     tl.from("#splide", {duration: 1, opacity: 0})
 
+    // Tentang
+    const stTentang = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".hero",
+            start: "bottom center",
+            end: "center top",
+            markers: true
+        }
+    });
+
+    stTentang.from(".tentang-desc .img", {x: -1000, opacity: 0, duration: 1})
+    stTentang.from(".tentang-desc .desc", {x: 1000, opacity: 0, duration: .5})
+    stTentang.from(".unggulan-title", {y: 25, opacity: 0, duration: .5})
+    stTentang.from(".unggulan-item", {y: 100, opacity: 0, duration: 1})
+
+    // Harga
+    const stHarga = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".unggulan",
+            start: "bottom center",
+            end: "center top",
+            markers: true
+        }
+    });
+    stHarga.from(".harga-global .harga-title", {y: 25, opacity: 0, duration: .5})
+    stHarga.from(".harga-global .paket", {y: 150, opacity: 0, duration: 1, delay: .5})
+
+    const stHargaAttack = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".harga-global",
+            start: "bottom center",
+            end: "center top",
+            markers: true
+        }
+    });
+    stHargaAttack.from(".harga-attack .harga-title", {y: 25, opacity: 0, duration: .5})
+    stHargaAttack.from(".harga-attack .paket", {y: 150, opacity: 0, duration: 1, delay: .5})
+
+
     new Splide( '#splide', {
         'cover'      : true,
         'heightRatio': 0.5,
